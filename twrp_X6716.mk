bb54c50ce9b9907996a681924e2d14365295b415
nonoobik <ib2279021@gmail.com>
2026-09-19 16:33:27 +0400

    Update twrp_X6716.mk

--- twrp_X6716.mk
+++ twrp_X6716.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from Infinix-X6716 device
-$(call inherit-product, device/infinix/Infinix-X6716/device.mk)
+$(call inherit-product, device/infinix/X6716/device.mk)
 
-PRODUCT_DEVICE := Infinix-X6716
-PRODUCT_NAME := omni_Infinix-X6716
+PRODUCT_DEVICE := X6716
+PRODUCT_NAME := twrp_X6716
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6716
 PRODUCT_MANUFACTURER := infinix
